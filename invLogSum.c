921099fc4458ab95e8a09c7d467916a33f15e290
fmeade <fmeade@radford.edu>
2016-03-18 14:17:13 -0400

    intial

---
+++ invLogSum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * invLogSum.c
+ *
+ * Proj3 - Concurrency via Threads
+ * Due: Thursday, March 24th, 2016 @ 2p
+ * Author: Forrest Meade (fmeade)
+ * 
+ * Description: 
+ */
+
+ int main(int argc, char** argv) {
+
+
+ 	return 0;
+ }
