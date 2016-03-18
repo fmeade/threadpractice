9b6c193d6a87c6451bb62c79045d6b14e9f7e701
fmeade <fmeade@radford.edu>
2016-03-18 14:30:00 -0400

    some error handling

--- invLogSum.c
+++ invLogSum.c
@@ -13,7 +13,33 @@
  * Help: 
  * 
  */
+/* The possible command-line options to the program. 
+ */
+struct option_info options[] =
+  {  { "num-threads",  'n',  "1", "the number of threads to create. " }
+    ,{ "stop",  's',  "2000000000", "the upper limit of the sum. " } 
+  };
+
+#define NUM_OPTIONS SIZEOF_ARRAY(options)
+
+
  int main(int argc, char** argv) {
+ 	char** settings = allOptions( argc, argv, NUM_OPTIONS, options );
+    // Now, the array `settings` contains all the options, in order:
+    // either taken from the command-line, or from the default given in `options[]`.
+
+    if(settings[0] < 1) {
+    	printf("%s\n", "ERROR: Not a positive number.");
+    	exit(-1);
+    }
+
+    if(settings[1] < 2) {
+    	printf("%s\n", "ERROR: Not larger than 2.");
+    	exit(-1);
+    }
+
+
+
 
 
  	return 0;
